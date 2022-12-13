0b04c906dd2a1be2d29cd81c85b9ce9c54409f17
Mamadou SARR <megatif.sarr@gmail.com>
2022-12-13 21:05:31 +0100

    video-12 Create Product Details Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
    switch (action.type) {
@@ -41,6 +42,9 @@ const HomeScreen = () => {
 
    return (
       <div>
+         <Helmet>
+            <title>Amazona</title>
+         </Helmet>
          <h1>Featured Products</h1>
          <div className='products'>
             {loading ? (
