f3e4ed7d5fb979f724b88be843a75d6d4e1bf0f1
Mamadou SARR <megatif.sarr@gmail.com>
2022-12-13 18:19:30 +0100

    video-11 Create Product and Rating Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 // import data from '../data';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
    switch (action.type) {
@@ -47,23 +48,13 @@ const HomeScreen = () => {
             ) : error ? (
                <div>{error}</div>
             ) : (
-               products.map((product) => (
-                  <div className='product' key={product.slug}>
-                     <Link to={`/product/${product.slug}`}>
-                        <img src={product.image} alt={product.name} />
-                     </Link>
-                     <div className='product-info'>
-                        <Link to={`/product/${product.slug}`}>
-                           <p>{product.name}</p>
-                        </Link>
-                        <p>
-                           <strong>$</strong>
-                           {product.price}
-                        </p>
-                        <button>Add to cart</button>
-                     </div>
-                  </div>
-               ))
+               <Row>
+                  {products.map((product) => (
+                     <Col key={product.slug} sm={6} md={4} lg={3} className='mb-3'>
+                        <Product product={product} />
+                     </Col>
+                  ))}
+               </Row>
             )}
          </div>
       </div>
